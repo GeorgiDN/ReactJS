d899fff468777b6af2695053cd9377def6fa4276
GeorgiDN <go6odn28@gmail.com>
2025-06-12 08:27:20 +0300

    components demo

--- ReactJsDemo-1/src/app.js
+++ ReactJsDemo-1/src/app.js
@@ -7,4 +7,40 @@ const secondHeadingReactElement = React.createElement('h2', null, 'JSX');
 
 const headingReactSectionElement = React.createElement('header', null, headingReactElement, secondHeadingReactElement);
 
-rootReactElement.render(headingReactSectionElement);
+
+function Main(props) {
+    return React.createElement(
+        'main',
+        {},
+        React.createElement('h3', {}, props.title),
+        React.createElement(
+            'li',
+            {},
+            'The Matrix',
+        ),
+        React.createElement(
+            'li',
+            {},
+            'Man of steel',
+        )
+    );
+}
+
+const siteContent = React.createElement(
+    'div',
+    {},
+    headingReactSectionElement,
+    React.createElement(
+        Main,
+        {title: 'Best Movies'},
+    )
+);
+
+const siteContentJSX = (
+    <div>
+        {headingReactSectionElement}
+        <Main title='Best Movies' />
+    </div>
+)
+
+rootReactElement.render(siteContentJSX);
